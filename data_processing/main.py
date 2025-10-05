13f32e8eec1e5c952995a48a33bc21bc039604ad
Aastha <asingla1101@gmail.com>
2025-10-05 07:20:38 +0530

    using groupby with the 3 different python libraraies

--- data_processing/main.py
+++ data_processing/main.py
@@ -3,3 +3,5 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("DemoApp").getOrCreate()
 df = spark.read.csv("labs.csv", header=True, inferSchema=True)
 df.show()
+
+df.groupBy("TestName").count().show()
